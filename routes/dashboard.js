5400179ed58033f575c160c19f5c3a17475d4ab7
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2022-01-10 15:45:15 +0100

    blabla

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const { redirect } = require('express/lib/response');
 const router = express.Router();
+
+//Artist routes
 const Artist = require('../models/artist.model')
 
 const renderArtistPage = async (req,res,next) => {
@@ -13,7 +15,6 @@ const renderArtistPage = async (req,res,next) => {
 }
 
 const creatArtist = async (req,res,next) => {
-    console.log(req.body);
     try {const creation = await Artist.create({
         name : req.body.name,
         isBand : req.body.isBand === "on"? true : false,
@@ -44,5 +45,32 @@ router.get('/delete/:id', (req,res) => {
         console.log(err);
     })
 });
+
+//labels routes
+
+const Label = require('../models/label.model')
+const renderLabelsList = async (req, res) => {
+    try{
+        const labels = await Label.find()
+        res.render('labels.hbs', {
+            labels : labels
+        });
+    }catch(err){
+        console.error(err)
+    }
+}
+
+const createLabel = async (res, req) =>{
+    console.log(req.body)
+    try{
+       await Label.create(req.body);
+        res.redirect('/dashboard/labels')
+    }catch(err){
+        console.error(err)
+    }
+}
+router.get('/labels',renderLabelsList )
+router.get('/labels/create', (req, res) => res.render('labels/createLabels'))
+router.post('/labels/create', createLabel);
 
 module.exports = router
